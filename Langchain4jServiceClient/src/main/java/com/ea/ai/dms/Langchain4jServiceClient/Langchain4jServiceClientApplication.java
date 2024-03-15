129719115a322cd041c349ce1331cb48c864d382
Emile ASTIH <emileastih1@gmail.com>
2024-03-15 14:36:42 +0100

    initial commit

---
+++ Langchain4jServiceClient/src/main/java/com/ea/ai/dms/Langchain4jServiceClient/Langchain4jServiceClientApplication.java
@@ -0,0 +1,13 @@
+package com.ea.ai.dms.Langchain4jServiceClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Langchain4jServiceClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Langchain4jServiceClientApplication.class, args);
+	}
+
+}
